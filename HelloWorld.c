c21da8c42553a50937e2b1f475d128ba39486f8c
Khushipitroda <64003946+Khushipitroda@users.noreply.github.com>
2021-03-14 10:55:50 +0530

    Update HelloWorld.c
    I've added some comments

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,5 +1,6 @@
-#include<stdio.h>
-void main()
+#include<stdio.h>    //preprocessor 
+/*The #include is a preprocessor command that tells the compiler to include the contents of stdio.h (standard input and output) file in the program.*/
+void main()          //main function from where program will start execution
 {
-   printf("Hello_World !");
+   printf("Hello_World !");   // printf() displays the string inside quotation
 }
